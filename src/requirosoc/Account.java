ac74777ab96ffadd612d58b65f383eb29e0c1b6c
Zeyuru <requirosoaaronr@gmail.com>
2024-09-09 15:28:41 +0800

    update

---
+++ src/requirosoc/Account.java
@@ -0,0 +1,35 @@
+
+package requirosoc;
+
+import java.util.Scanner;
+
+
+public class Account {
+    class Acc{
+        int id;
+        String FirstName;
+        String LastName;
+        String Email;
+        String Username;
+        String Password;
+        
+        Acc(int id, String FirstName, String LastName, String Email, String Username, String Password){
+            this.id = id;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.Username = Username;
+            this.Password = Password;
+            
+        }
+    }
+    
+}
+
+
+ 
+
+
+
+
+
